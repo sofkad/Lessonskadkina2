56239fd95fd98ff331c3569470915985031821b8
sofkad <121443608+sofkad@users.noreply.github.com>
2022-12-26 10:28:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import math
+
+#print ("enter your name: ")
+#name = input ()
+
+#print ("enter your last_name: ")
+#last_name =  input ()
+
+#print (f"Your full name is {name} {last_name}")
+#print ("Your full name is " + name +    last_name)
+#print ("Your full name is {} {} ".format(name, last_name))
+
+#print ([name.upper()] + [last_name.upper()])
+#print ([name.lower()] + [last_name.lower()])
+#print ([name.title()] + [last_name.title()])
+#print ([name.capitalize()] + [last_name.capitalize()])8
+
+#full_name = "Sofia Kadkina"
+#print (full_name.isalnum())
+#print (full_name.isalpha())
+#print (full_name.isdigit())
+#print (full_name.split())
+#print (full_name.rstrip())
+
+#print ("\n\tfull_name")
+
+#from math import pi
+#r = float(input ("Input the radius of the circle : "))
+#print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+usd = 36.9132
+uah = float(input ("uah: "))
+currency=uah/usd
+print ("Current stock exchange rate is " + str(usd) + " forms: " + str(round(currency, 2)) +  "\tusd")
+
